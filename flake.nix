5a893c1b5814a0ab3b6acd649d0c0ff5d8c2d007
Adophilus <uchenna19of@gmail.com>
2024-07-14 10:50:13 +0100

    added nxi flake file

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My gleam implementation of an auth server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gleam
+          erlang
+          rebar3
+        ];
+      };
+    };
+  };
+}
